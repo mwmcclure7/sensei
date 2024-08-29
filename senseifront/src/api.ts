790da2dc4bc326d62a7b54ff106dfc1ea57547e6
mwmcclure7 <106849204+mwmcclure7@users.noreply.github.com>
2024-08-28 23:13:50 -0400

    added apiurl

--- senseifront/src/api.ts
+++ senseifront/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : "/choreo-apis/sensei/senseiback/v1",
 });
 
 api.interceptors.request.use(
