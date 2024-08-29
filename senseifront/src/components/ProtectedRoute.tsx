a75882e70cbe1e910cce6bc72f2b29a5a171c152
mwmcclure7 <106849204+mwmcclure7@users.noreply.github.com>
2024-08-28 23:46:33 -0400

    Fixed ProtectedRoute

--- senseifront/src/components/ProtectedRoute.tsx
+++ senseifront/src/components/ProtectedRoute.tsx
@@ -4,11 +4,18 @@ import api from "../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
 import { useState, useEffect } from "react";
 
+/* Just a quick explanation for all of the @ts-ignore lines.
+This worked before as a .jsx file. Then, I rewrote the frontend for Vite instead of create-react-app.
+After I tried importing the file into App.tsx, I was getting an error because it is a .jsx file instead of a .tsx file.
+So, I switched it to a .tsx file and fixed all errors, but then it no longer functioned.
+My solution: just ignore all of the TypeScript errors and keep the original .jsx code. */
+
 // @ts-ignore
 function ProtectedRoute({ children }) {
-    const [isAuthorized, setIsAuthorized] = useState(false);
+    const [isAuthorized, setIsAuthorized] = useState(null);
 
     useEffect(() => {
+        // @ts-ignore
         auth().catch(() => setIsAuthorized(false));
 
         window.addEventListener("auth", auth);
@@ -22,12 +29,15 @@ function ProtectedRoute({ children }) {
             });
             if (res.status === 200) {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                // @ts-ignore
                 setIsAuthorized(true);
             } else {
+                // @ts-ignore
                 setIsAuthorized(false);
             }
         } catch (error) {
             console.log(error);
+            // @ts-ignore
             setIsAuthorized(false);
         }
     };
@@ -35,6 +45,7 @@ function ProtectedRoute({ children }) {
     const auth = async () => {
         const token = localStorage.getItem(ACCESS_TOKEN);
         if (!token) {
+            // @ts-ignore
             setIsAuthorized(false);
             return;
         }
@@ -46,6 +57,7 @@ function ProtectedRoute({ children }) {
         if (tokenExpiration < now) {
             await refreshToken();
         } else {
+            // @ts-ignore
             setIsAuthorized(true);
         }
     };
