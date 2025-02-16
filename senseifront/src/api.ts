fd9ac8e374c5b26f50727fdf6f3a05df6edf9aa1
mwmcclure7 <106849204+mwmcclure7@users.noreply.github.com>
2025-02-15 21:13:14 -0500

    Removed choreo backend link

--- senseifront/src/api.ts
+++ senseifront/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : "/choreo-apis/sensei/senseiback/v1",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
